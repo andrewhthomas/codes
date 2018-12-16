0a5a65338c2693c64cc55e36f0364b9f9e5702d7
Andrew Thomas <andrewhthomas@gmail.com>
2018-12-16 13:34:02 -0500

    Initial work on index page

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,5 +1,35 @@
 @extends('_layouts.master')
 
 @section('body')
-<h1>Hello world!</h1>
+<section class="hero has-background-black">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">Hi there!
+        <br>I&rsquo;m Andrew Thomas. A&nbsp;Drupal developer living in Philadelphia, currently working at
+        <a
+          class="underline--magical"
+          href="#">Message&nbsp;Agency</a>.
+      </h1>
+    </div>
+  </div>
+</section>
+
+<section
+  class="andrew"
+  style="">
+  <div/>
+</section>
+
+<section class="hero has-background-link">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">Hi there!
+        <br>I'm Andrew Thomas. A&nbsp;Drupal developer living in Philadelphia, currently working at
+        <a
+          class="underline--magical"
+          href="#">Message&nbsp;Agency</a>.
+      </h1>
+    </div>
+  </div>
+</section>
 @endsection
